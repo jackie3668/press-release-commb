af06a164c0a5e34db45fdc739a1051032ff0fa26
Jackie <actions@github.com>
2024-07-18 11:03:54 -0400

    Finished Styling

--- src/Components/FormTemplate.jsx
+++ src/Components/FormTemplate.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { db, storage } from '../firebase'; // Import Firebase db and storage
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import './FormTemplate.css'; // CSS for styling (if any)
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import logo from './image1.png'; // Placeholder logo image
 
 const FormTemplate = () => {
@@ -14,17 +13,21 @@ const FormTemplate = () => {
     personTitle: 'Insert Person Title',
     quote: 'Insert Quote',
     boilerplate: 'Insert Boilerplate',
-    companyLogoURL: null, 
-    // Added companyLogoURL state to store the download URL
+    companyLogoURL: null,
+    logoWidth: null, // Added logoWidth state
+    logoHeight: null, // Added logoHeight state
   });
   const [imageURL, setImageURL] = useState(null);
   const [editField, setEditField] = useState(null);
+  const [widthTemp, setWidthTemp] = useState(null);
+  const [heightTemp, setHeightTemp] = useState(null);
 
   const handleInputChange = (key, value) => {
     setFormData({
       ...formData,
       [key]: value,
     });
+    document.getElementById(key).classList.add('edited')
   };
 
   const handleFieldClick = (field) => {
@@ -34,78 +37,88 @@ const FormTemplate = () => {
   const handleSave = async () => {
     try {
       console.log('Starting handleSave...');
-      
+
       // Ensure formData is correctly populated
       console.log('formData:', formData);
-  
+
       const timestamp = serverTimestamp(); // Firestore server timestamp
       console.log('Timestamp:', timestamp);
-  
-        const storageBucket = 'press-release-24'; // Replace with your Firebase Storage bucket name
-        const companyName = formData.companyName; // Assuming formData contains companyName
-        const path = 'gs://' + storageBucket + '/images/' + companyName; // Full path including bucket and filename
-      
+
+      const storageBucket = 'press-release-24'; // Replace with your Firebase Storage bucket name
+      const companyName = formData.companyName; // Assuming formData contains companyName
+      const path = 'gs://' + storageBucket + '/images/' + companyName; // Full path including bucket and filename
+
       const storageRef = ref(storage, path);
-        // Upload file to Firebase Storage
-        const snapshot = await uploadBytes(storageRef, formData.companyLogoPNG);
-        console.log('File uploaded successfully:', snapshot);
-  
-        // Get download URL
-        const downloadURL = await getDownloadURL(snapshot.ref);
-        console.log('Download URL:', downloadURL);
-  
-        // Update companyLogoURL in formData and state
-        console.log('Before updating formData:', formData.companyLogoURL);
-        setFormData({
-          ...formData,
-          companyLogoURL: downloadURL, // Ensure downloadURL is correctly obtained
-        });
-        
-        await setDoc(doc(db, 'press-release', formData.companyName), {
-          companyName: formData.companyName,
-          companyDescription: formData.companyDescription,
-          companyOfferings: formData.companyOfferings,
-          personName: formData.personName,
-          personTitle: formData.personTitle,
-          quote: formData.quote,
-          boilerplate: formData.boilerplate,
-          companyLogoURL: downloadURL,
-          timestamp: timestamp,
-        });
-    
-        console.log('After updating formData:', formData.companyLogoURL);
-        // Log updated formData
-        console.log('Updated formData:', formData.companyLogoURL);
-  
-        // Alert user
-        alert('Image uploaded successfully!');
+      // Upload file to Firebase Storage
+      const snapshot = await uploadBytes(storageRef, formData.companyLogoPNG);
+      console.log('File uploaded successfully:', snapshot);
 
+      // Get download URL
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      console.log('Download URL:', downloadURL);
 
-      console.log('Form data saved successfully to Firestore!');
-  
-      // Alert user
+      // Update companyLogoURL in formData and state
+      console.log('Before updating formData:', formData.companyLogoURL);
+      setFormData({
+        ...formData,
+        companyLogoURL: downloadURL, // Ensure downloadURL is correctly obtained
+      });
+
+      await setDoc(doc(db, 'press-release', formData.companyName), {
+        companyName: formData.companyName,
+        companyDescription: formData.companyDescription,
+        companyOfferings: formData.companyOfferings,
+        personName: formData.personName,
+        personTitle: formData.personTitle,
+        quote: formData.quote,
+        boilerplate: formData.boilerplate,
+        companyLogoURL: imageURL,
+        logoWidth: widthTemp, 
+        logoHeight: heightTemp, 
+        timestamp: timestamp,
+      });
+
       alert('Form submitted!');
     } catch (error) {
+      alert('Error saving data!');
       console.error('Error saving form data: ', error);
     }
   };
-  
 
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     if (file.type === 'image/png') {
-      // Set companyLogoPNG in formData
-      setFormData({
-        ...formData,
-        companyLogoPNG: file,
-      });
-
-      // Optionally store the file object if needed
-
-      // Get download URL for the uploaded file
       const reader = new FileReader();
       reader.onload = function (e) {
-        setImageURL(e.target.result);
+        const img = new Image();
+        img.onload = function () {
+          const maxWidth = 400; // Maximum width allowed
+          const aspectRatio = img.naturalWidth / img.naturalHeight;
+          const width = Math.min(img.naturalWidth, maxWidth);
+
+          const height = width / aspectRatio;
+          // Create a canvas element to resize the image
+          const canvas = document.createElement('canvas');
+          canvas.width = width;
+          canvas.height = height;
+
+          const ctx = canvas.getContext('2d');
+          ctx.drawImage(img, 0, 0, width, height);
+          setWidthTemp(width);
+          setHeightTemp(height);
+
+          // Convert canvas back to blob
+          canvas.toBlob((blob) => {
+            const resizedFile = new File([blob], file.name, {
+              type: 'image/png',
+              lastModified: Date.now(),
+            });
+
+            // Optionally store the file object if needed
+            setImageURL(canvas.toDataURL('image/png'));
+          }, 'image/png');
+        };
+        img.src = e.target.result;
       };
       reader.readAsDataURL(file);
     } else {
@@ -114,25 +127,25 @@ const FormTemplate = () => {
   };
 
   return (
-    <div>
+    <div className='container'>
       <div className="logos">
         <img src={logo} alt="" />
-        <label>
-          Upload Image:
-          <input type="file" accept="image/*" onChange={handleImageUpload} />
-          {imageURL && (
+        {imageURL && (
             <img
               src={imageURL}
               alt="Uploaded Image"
               style={{ maxWidth: '100%', marginTop: '10px' }}
             />
           )}
+        <label>
+          Upload PNG Logo:
+          <input type="file" accept="image/*" onChange={handleImageUpload} />
         </label>
       </div>
-      <h1>NEW MEMBER PRESS RELEASE</h1>
+      <h2>NEW MEMBER PRESS RELEASE</h2>
       <p>
         The Canadian Out-of-Home Marketing and Measurement Bureau (COMMB) has added to their roster of association members, which now includes{' '}
-        <span className='clickable' onClick={() => handleFieldClick('companyName')}>
+        <span id='companyName' className="clickable" onClick={() => handleFieldClick('companyName')}>
           {editField === 'companyName' ? (
             <input
               type="text"
@@ -148,11 +161,11 @@ const FormTemplate = () => {
       </p>
       <p>
         {formData.companyName} is a{' '}
-        <span className='clickable' onClick={() => handleFieldClick('companyDescription')}>
+        <span id='companyDescription' className="clickable" onClick={() => handleFieldClick('companyDescription')}>
           {editField === 'companyDescription' ? (
-            <input
-              type="text"
+            <textarea
               value={formData.companyDescription}
+              rows={1}
               onChange={(e) => handleInputChange('companyDescription', e.target.value)}
               onBlur={() => setEditField(null)}
               autoFocus
@@ -161,10 +174,9 @@ const FormTemplate = () => {
             formData.companyDescription
           )}
         </span>. Their network includes{' '}
-        <span className='clickable' onClick={() => handleFieldClick('companyOfferings')}>
+        <span id='companyOfferings' className="clickable" onClick={() => handleFieldClick('companyOfferings')}>
           {editField === 'companyOfferings' ? (
-            <input
-              type="text"
+            <textarea
               value={formData.companyOfferings}
               onChange={(e) => handleInputChange('companyOfferings', e.target.value)}
               onBlur={() => setEditField(null)}
@@ -176,7 +188,7 @@ const FormTemplate = () => {
         </span>.
       </p>
       <p>
-        <span className='clickable' onClick={() => handleFieldClick('personName')}>
+        <span id='personName' className="clickable" onClick={() => handleFieldClick('personName')}>
           {editField === 'personName' ? (
             <input
               type="text"
@@ -188,8 +200,8 @@ const FormTemplate = () => {
           ) : (
             formData.personName
           )}
-        </span>, {' '}
-        <span className='clickable' onClick={() => handleFieldClick('personTitle')}>
+        </span>,{' '}
+        <span id='personTitle' className="clickable" onClick={() => handleFieldClick('personTitle')}>
           {editField === 'personTitle' ? (
             <input
               type="text"
@@ -201,12 +213,12 @@ const FormTemplate = () => {
           ) : (
             formData.personTitle
           )}
-        </span>, shares their excitement about joining COMMB.{' '}{formData.personName}{' '}states,{' "'}
-        <span className='clickable' onClick={() => handleFieldClick('quote')}>
+        </span>, shares their excitement about joining COMMB. {formData.personName} states, "{''}
+        <span id='quote' className="clickable" onClick={() => handleFieldClick('quote')}>
           {editField === 'quote' ? (
-            <input
-              type="text"
+            <textarea
               value={formData.quote}
+              rows={1}
               onChange={(e) => handleInputChange('quote', e.target.value)}
               onBlur={() => setEditField(null)}
               autoFocus
@@ -215,21 +227,25 @@ const FormTemplate = () => {
             formData.quote
           )}
         </span>
-        {'".'}
+        ".
       </p>
       <p>
-        COMMB, committed to providing measurement and marketing solutions for the Canadian OOH industry, is excited to welcome{' '}
-        {formData.companyName} to its membership. They look forward to collaborating closely with their team across various facets of the OOH landscape.
+        COMMB, committed to providing measurement and marketing solutions for the Canadian OOH industry, is excited to welcome {formData.companyName} to its membership. They look forward to collaborating closely with their team across various facets of the OOH landscape.
+      </p>
+      <p>
+        <strong>About COMMB</strong>
+        <br />
+        COMMB is the national not-for-profit organization for the Canadian out-of-home (OOH) industry. Our membership base is comprised of advertisers, agencies, programmatic tech stacks, and OOH companies, large and small. COMMB is responsible for the collective marketing and measurement efforts for the OOH industry, developing proprietary audience measurement methodologies for a variety of OOH media formats, and ensuring the voice of OOH is at the forefront of media via broad marketing and communications initiatives.
       </p>
       <p>
         <strong>About </strong>
         {formData.companyName}
         <br />
-        <span className='clickable' onClick={() => handleFieldClick('boilerplate')}>
+        <span id='boilerplate' className="clickable" onClick={() => handleFieldClick('boilerplate')}>
           {editField === 'boilerplate' ? (
-            <input
-              type="text"
+            <textarea
               value={formData.boilerplate}
+              rows={1}
               onChange={(e) => handleInputChange('boilerplate', e.target.value)}
               onBlur={() => setEditField(null)}
               autoFocus
@@ -248,8 +264,7 @@ const FormTemplate = () => {
         <br />
         jcopeland@commb.ca
       </p>
-      {/* Save button (for demonstration, adjust functionality as needed) */}
-      <button onClick={handleSave}>Save</button>
+      <button onClick={handleSave}>Submit</button>
     </div>
   );
 };
--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { db } from '../firebase'; // Import your Firestore instance
 import { collection, getDocs } from 'firebase/firestore'; // Import Firestore methods
-import { Document, Packer, Paragraph, TextRun, AlignmentType, ExternalHyperlink, ImageRun} from 'docx';
+import { Document, Packer, Paragraph, TextRun, ImageRun, ExternalHyperlink } from 'docx';
 import { saveAs } from 'file-saver';
-import { getDownloadURL, ref } from "firebase/storage";
-import logo from './image1.png'
+import logo from './image1.png';
 
 const Forms = () => {
   const [forms, setForms] = useState([]);
-  const [fileData, setFileData] = useState(null);
 
   useEffect(() => {
     const fetchForms = async () => {
       try {
         const formsCollection = collection(db, 'press-release');
         const snapshot = await getDocs(formsCollection);
-        const formsData = snapshot.docs.map(doc => ({
+        const formsData = snapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
-          timestamp: doc.data().timestamp.toDate().toLocaleString() // Convert Firestore timestamp to a readable format
+          timestamp: doc.data().timestamp.toDate().toLocaleString(), // Convert Firestore timestamp to a readable format
         }));
         setForms(formsData);
       } catch (error) {
@@ -29,29 +27,18 @@ const Forms = () => {
     fetchForms();
   }, []);
 
-
-
   const handleExport = async (formData) => {
     try {
-      // Fetch logo and convert to array buffer
-      const response = await fetch(logo); // Replace 'logo' with your actual logo URL or path
-      const blob1 = await response.blob();
+      // Fetch logo and convert to array buffer (Replace with actual logo URL or path)
+      const response1 = await fetch(logo); // Assuming 'logo' is your placeholder image path
+      const blob1 = await response1.blob();
       const arrayBuffer1 = await blob1.arrayBuffer();
-  
-      // Fetch company logo and convert to array buffer
+
+      // Fetch company logo and convert to base64
       const imageUrl = formData.companyLogoURL;
-      const fetchOptions = {
-        method: 'GET',
-        headers: {},
-        mode: 'cors',
-        credentials: 'same-origin',
-      };
-  
-      // Fetch company logo image and convert to array buffer
-      const response2 = await fetch(imageUrl, fetchOptions);
+      const response2 = await fetch(imageUrl);
       const blob2 = await response2.blob();
       const arrayBuffer2 = await blob2.arrayBuffer();
-  
       // Construct document with images and text
       const doc = new Document({
         sections: [
@@ -61,22 +48,20 @@ const Forms = () => {
               new Paragraph({
                 alignment: 'left',
                 children: [
-                  // First image with arrayBuffer1
                   new ImageRun({
-                    type: 'png',
+                    type: 'png', 
                     data: arrayBuffer1,
                     transformation: {
-                      width: 100,
+                      width: 300,
                       height: 100,
                     },
                   }),
-                  // Second image with arrayBuffer2
                   new ImageRun({
-                    type: 'png',
+                    type: 'png', // Adjust the type as per your image type
                     data: arrayBuffer2,
                     transformation: {
-                      width: 100,
-                      height: 100,
+                      width: formData.logoWidth,
+                      height: formData.logoHeight,
                     },
                   }),
                 ],
@@ -84,14 +69,13 @@ const Forms = () => {
                   after: 200,
                 },
               }),
-              // Other paragraphs and text runs
               new Paragraph({
                 alignment: 'left',
                 children: [
                   new TextRun({
-                    text: "NEW MEMBER PRESS RELEASE",
+                    text: 'NEW MEMBER PRESS RELEASE',
                     bold: true,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 26,
                   }),
                 ],
@@ -104,7 +88,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `The Canadian Out-of-Home Marketing and Measurement Bureau (COMMB) has added to their roster of association members, which now includes ${formData.companyName}.`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -117,7 +101,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `${formData.companyName} is a ${formData.companyDescription}. Their network includes ${formData.companyOfferings}.`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -130,7 +114,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `${formData.personName}, ${formData.personTitle}, shares their excitement about joining COMMB. ${formData.personName} states, "${formData.quote}".`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -143,7 +127,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `COMMB, committed to providing measurement and marketing solutions for the Canadian OOH industry, is excited to welcome ${formData.companyName} to its membership. They look forward to collaborating closely with their team across various facets of the OOH landscape.`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -155,9 +139,9 @@ const Forms = () => {
                 alignment: 'left',
                 children: [
                   new TextRun({
-                    text: "About COMMB",
+                    text: 'About COMMB',
                     bold: true,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -170,7 +154,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `COMMB is the national not-for-profit organization for the Canadian out-of-home (OOH) industry. Our membership base is comprised of advertisers, agencies, programmatic tech stacks, and OOH companies, large and small. COMMB is responsible for the collective marketing and measurement efforts for the OOH industry, developing proprietary audience measurement methodologies for a variety of OOH media formats, and ensuring the voice of OOH is at the forefront of media via broad marketing and communications initiatives.`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -183,13 +167,13 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `About `,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                     bold: true,
                   }),
                   new TextRun({
                     text: `${formData.companyName}`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -202,7 +186,7 @@ const Forms = () => {
                 children: [
                   new TextRun({
                     text: `${formData.boilerplate}`,
-                    font: "Arial",
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -214,8 +198,19 @@ const Forms = () => {
                 alignment: 'left',
                 children: [
                   new TextRun({
-                    text: `For more information, please contact:`,
-                    font: "Arial",
+                    text: 'For more information, please contact:',
+                    font: 'Arial',
+                    size: 22,
+                    bold: true
+                  }),
+                ],
+              }),
+              new Paragraph({
+                alignment: 'left',
+                children: [
+                  new TextRun({
+                    text: 'Jennifer Copeland',
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
@@ -224,88 +219,52 @@ const Forms = () => {
                 alignment: 'left',
                 children: [
                   new TextRun({
-                    text: `Jennifer Copeland`,
-                    font: "Arial",
-                    size: 22,
-                  }),
-                ],
-              }),
-              new Paragraph({
-                alignment: 'left',
-                children: [
-                  new TextRun({
-                    text: `Director, Brand Communications`,
-                    font: "Arial",
+                    text: 'Director, Brand Communications',
+                    font: 'Arial',
                     size: 22,
                   }),
                 ],
               }),
               new Paragraph({
                 children: [
-                  new TextRun({
-                    text: "jcopeland@commb.ca",
-                    style: "Hyperlink",
-                    size: 22,
-                    font: "Arial",
-                  }),
+                    new ExternalHyperlink({
+                        children: [
+                            new TextRun({
+                                text: "jcopeland@commb.ca",
+                                style: "Hyperlink",
+                                font: 'Arial',
+                                size: 22,
+                                underline: true,
+                            }),
+                        ],
+                        link: 'mailto:jcopeland@commb.ca'
+                    }),
                 ],
               }),
+            
             ],
           },
         ],
       });
-  
+
       // Pack the document into a blob and save
       const docBlob = await Packer.toBlob(doc);
       saveAs(docBlob, 'output.docx');
-      console.log('Document created successfully');
       console.log('Document created successfully');
     } catch (error) {
       console.error('Error generating document: ', error);
     }
   };
 
-  const handleImage = async (formData) => {
-    const imageElement = document.getElementById(formData.companyName);
-    console.log(imageElement);
-  };
-  
-  // const fetchImageAsUint8Array = async (imageUrl, name) => {
-  //   console.log(imageUrl, name);
-  //   try {
-  //     // Create a reference to the file you want to download
-  //      const fileRef = ref(storage, `images/${companyName}`);
-
-  //     // Get the download URL
-  //     const url = await getDownloadURL(fileRef);
-  //     setFileUrl(url);
-  //   } catch (error) {
-  //     console.error("Error fetching file:", error);
-  //   }
-  // };
-  
-  
   return (
-    <div>
-      <h2>Forms Data</h2>
+    <div className='export'>
+      <h2>Submitted PR</h2>
       <table border="1">
-        <thead>
-          <tr>
-            {forms.length > 0 &&
-              Object.keys(forms[0]).map((key, index) => (
-                <th key={index}>{key}</th>
-              ))}
-            <th>Actions</th>
-          </tr>
-        </thead>
         <tbody>
           {forms.map((form, rowIndex) => (
             <tr key={rowIndex}>
-              {Object.values(form).map((value, colIndex) => (
-                <td key={colIndex}>{value}</td>
-              ))}
+              <td>{form.companyName}</td>
               <td>
-                <img id={form.companyName} src={form.companyLogoURL ?form.companyLogoURL : '' } alt="" />
                 <button onClick={() => handleExport(form)}>Export</button>
               </td>
             </tr>
